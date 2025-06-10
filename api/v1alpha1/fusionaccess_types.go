11de656239f11bd94e3633acdcd4e2ec3609d66a
Michele Baldessari <michele@acksyn.org>
2025-06-10 18:41:39 +0200

    Recreate enums

--- api/v1alpha1/fusionaccess_types.go
+++ api/v1alpha1/fusionaccess_types.go
@@ -22,7 +22,7 @@ import (
 
 // NOTE(bandini): If you change anything in the following two lines you need to update
 // ./scripts/update-cnsa-versions-metadata.sh
-// +kubebuilder:validation:Enum=v5.2.3.0.1
+// +kubebuilder:validation:Enum=v5.2.3.0.1;v5.2.3.0.dummy
 type StorageScaleVersions string
 
 // FusionAccessSpec defines the desired state of FusionAccess
