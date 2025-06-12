e1f6943ad8fe970945b7e4a55b78742fbd0aaf0f
Michele Baldessari <michele@acksyn.org>
2025-06-12 16:07:57 +0200

    start with release script

---
+++ scripts/konflux-release.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+set -e -o pipefail
+
+get_last_git_merge_commit() {
+    git log --merges -n 1 --pretty=format:"%H"
+}
+
+PROJ="storage-scale-releng-tenant"
+DEST_REGISTRY="quay.io/rhn_support_mbaldess"
+
+if [ -z "$1" ]; then
+  # COMMIT=$(get_last_git_merge_commit)
+  # echo "Using the last merge commit automatically: ${COMMIT}"
+  echo "Please pass the merge commit you want to use: ./$0 <mergecommit>"
+  exit 1
+fi
+COMMIT="${1}"
+echo "User provided commit: ${COMMIT}"
+
+if ! oc project "${PROJ}" &> /dev/null; then
+  echo "Cannot access project ${PROJ}"
+  echo "Make sure you are logged in via: https://oauth-openshift.apps.stone-prd-rh01.pg1f.p1.openshiftapps.com/oauth/token/display"
+  exit 1
+fi
+
+applicationName=operator-0-1
+releaseVersion=$(echo $applicationName | sed 's/operator//g')
+operator_bundle=$(oc get components -ojsonpath='{range .items[?(@.spec.application=="'$applicationName'")]}{.metadata.name}{"\n"}{end}' | grep bundle)
+
+unset components
+declare -A components
+componentNames=($(oc get components -ojsonpath='{range .items[?(@.spec.application=="'$applicationName'")]}{.metadata.name}{"\n"}{end}'))
+for name in ${componentNames[@]}; do
+  genericName=$(echo $name | sed 's/'$releaseVersion'//g')
+  components[$genericName]=$name
+done
+
+SNAPSHOT=$(oc get snapshots --sort-by .metadata.creationTimestamp \
+  -l appstudio.openshift.io/component=$operator_bundle  \
+  -l pac.test.appstudio.openshift.io/event-type=push \
+  -l pac.test.appstudio.openshift.io/sha=${COMMIT} \
+  -ojsonpath='{range .items[*]}{@.metadata.name}{"\n"}{end}')
+
+SNAPSHOT_COUNT=$(echo "${SNAPSHOT}" | grep -c '^')
+
+if [ "${SNAPSHOT_COUNT}" -gt 1 ]; then
+  echo "Error: More than one matching snapshot found:" >&2
+  echo "${SNAPSHOT}" >&2
+  exit 1
+fi
+
+if [ "${SNAPSHOT_COUNT}" -eq 0 ]; then
+  echo "Error: No matching snapshot found." >&2
+  exit 1
+fi
+
+bundle=$(oc get snapshot ${SNAPSHOT} -ojsonpath='{.spec.components[?(@.name=="'$operator_bundle'")].containerImage}')
+releaseLabelInBundle=$(skopeo inspect docker://$bundle --format "{{.Labels.release}}")
+echo "Using: ${SNAPSHOT}"
+echo " bundle: ${bundle}"
+echo " release label: ${releaseLabelInBundle}"
+
+unset errors
+declare -A errors
+echo "Checking hashes:"
+for genericName in "${!components[@]}"; do
+  versionedName="${components[$genericName]}"
+  if [[ "$versionedName" == "$operator_bundle" ]]; then
+    continue
+  fi
+  echo -n "$versionedName "
+  echo -n "- Image digest: "
+  imagePullSpecInSnapshot=$(oc get snapshot ${SNAPSHOT} -ojsonpath='{.spec.components[?(@.name=="'$versionedName'")].containerImage}')
+  imageSHAInSnapshot=$(awk -F'@' '{print $2}' <<< "$imagePullSpecInSnapshot")
+  imagePullSpecInBundle=$(skopeo inspect docker://$bundle --format '{{ index .Labels "'$genericName'" }}')
+  imageSHAInBundle=$(awk -F'@' '{print $2}' <<< "$imagePullSpecInBundle")
+  if [ -n "$imagePullSpecInSnapshot" ] && [ "$imageSHAInBundle" = "$imageSHAInSnapshot" ]; then
+    echo -n "${imageSHAInBundle}"
+  else
+    errors+="$versionedName has digest $imageSHAInBundle in bundle but in snapshot it is $imageSHAInSnapshot \n";
+    echo -n "Mismatch 👎"
+  fi
+  echo -n -e " - Release version: "
+  imagePullSpecInSnapshot=$(oc get snapshot ${SNAPSHOT} -ojsonpath='{.spec.components[?(@.name=="'$versionedName'")].containerImage}')
+  releaseLabelInComponent=$(skopeo inspect docker://$imagePullSpecInSnapshot --format "{{.Labels.release}}")
+  if [ -n "$imagePullSpecInSnapshot" ] && [ "$releaseLabelInBundle" = "$releaseLabelInComponent" ]; then
+    echo "$releaseLabelInBundle"
+  else
+    errors+="$versionedName has release $releaseLabelInBundle in bundle but in snapshot it is $releaseLabelInComponent \n";
+    echo "Mismatch 👎"
+  fi
+done
+echo ""
+if [[ ${#errors[@]} == 0 ]];then
+  echo "OK: snapshot ${SNAPSHOT} image pullspecs and release versions match with bundle's labels"
+else
+  echo "ERROR: This snapshot is not a good candidate for a release:"
+  for error in ${(v)errors}; do
+    echo -e $error
+  done
+  exit 1
+fi
+
+echo "Copying images to new location:"
+
+for genericName in "${!components[@]}"; do
+  versionedName="${components[$genericName]}"
+  if [[ "$versionedName" == "$operator_bundle" ]]; then
+    continue
+  fi
+  SOURCEIMAGE=$(oc get snapshot ${SNAPSHOT} -ojsonpath='{.spec.components[?(@.name=="'$versionedName'")].containerImage}')
+  # quay.io/redhat-user-workloads/storage-scale-releng-tenant/must-gather-rhel9@sha256:efab18bf6e451624c1146cd04c672ae64a300fc2611b8dc10c0c1287607e976e
+  # Extract repo name (everything after the last slash before @)
+  REPO=$(basename "${SOURCEIMAGE%%@*}")
+  DIGEST="${SOURCEIMAGE##*@}"
+  DESTIMAGE="${DEST_REGISTRY}/${REPO}@${DIGEST}"
+
+  echo "Uploading ${SOURCEIMAGE} to ${DESTIMAGE}"
+  skopeo copy docker://${SOURCEIMAGE} docker://${DESTIMAGE}
+done
