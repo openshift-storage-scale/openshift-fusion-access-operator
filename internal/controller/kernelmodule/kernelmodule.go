bbbb56c23b1f4280abe9bf1ef38a00f58b0d30a9
Michele Baldessari <michele@acksyn.org>
2025-06-12 09:21:05 +0200

    Drop defaults as they are not realistic anyways

--- internal/controller/kernelmodule/kernelmodule.go
+++ internal/controller/kernelmodule/kernelmodule.go
@@ -175,8 +175,8 @@ func getIBMCoreImage(ctx context.Context, cl client.Client) (string, error) {
 }
 
 func NewDockerConfigmap(namespace string) *corev1.ConfigMap {
-	dockerFileValue := `ARG IBM_SCALE=quay.io/rhsysdeseng/cp/spectrum/scale/ibm-spectrum-scale-core-init@sha256:fde69d67fddd2e4e0b7d7d85387a221359daf332d135c9b9f239fb31b9b82fe0
-ARG DTK_AUTO=quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:01e0e07cc6c41638f8e9022fb9aa36a7984efcde2166d8158fb59a6c9f7dbbdf
+	dockerFileValue := `ARG IBM_SCALE
+ARG DTK_AUTO
 ARG KERNEL_FULL_VERSION
 FROM ${IBM_SCALE} as src_image
 FROM ${DTK_AUTO} as builder
