6e876c6bd62232bc9116a8a95eb4e0d06eed0898
Michele Baldessari <michele@acksyn.org>
2025-07-31 08:32:56 +0200

    Print errors out to stderr

--- cmd/devicefinder/main.go
+++ cmd/devicefinder/main.go
@@ -7,19 +7,19 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: devicefinder discover")
+		fmt.Fprintln(os.Stderr, "Usage: devicefinder discover")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
 	case "discover":
 		if err := startDeviceDiscovery(); err != nil {
-			fmt.Println(err)
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 	default:
-		fmt.Printf("Unknown command: %s\n", os.Args[1])
-		fmt.Println("Usage: devicefinder discover")
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", os.Args[1])
+		fmt.Fprintln(os.Stderr, "Usage: devicefinder discover")
 		os.Exit(1)
 	}
 }
