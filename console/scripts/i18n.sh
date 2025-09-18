d6d882e1a56fe88b3f3646006758a80522f62cf7
Jonathan Kilzi <jkilzi@redhat.com>
2025-09-18 13:48:59 +0300

    fix(i18n): update localization check to ensure files are up to date
    This commit modifies the localization script to correctly check for untracked files in the 'locales' directory. The condition is inverted to ensure that an error message is displayed when localization files are not up to date, prompting the user to run the i18n command.
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- console/scripts/i18n.sh
+++ console/scripts/i18n.sh
@@ -6,9 +6,11 @@ set -euo pipefail
 
 npx i18next -c config/i18next-parser.config.mjs
 npx ts-node scripts/i18n/set-english-defaults.ts
+git status --short --untracked-files -- locales
 
-if git status --short --untracked-files -- locales; then
-  echo "Localization files are not up to date. Run 'npm run i18n' then commit changes."
+handle_error() {
+  echo "Localization files are not up-to-date. Run 'npm run i18n' then commit changes."
   git --no-pager diff locales
-  exit 1
-fi
+}
+
+trap 'handle_error' ERR
