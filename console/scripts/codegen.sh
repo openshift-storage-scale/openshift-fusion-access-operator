067ca5955b598d7a69b400f94ed81a230746c3e3
Jonathan Kilzi <jkilzi@redhat.com>
2025-10-23 16:28:34 +0300

    fix: correct typo in WELL_KNOWN_CRDS for FileSystemClaim
    - Updated the WELL_KNOWN_CRDS array in codegen.sh to correct the spelling of 'FileSystemClaim' for consistency and accuracy.

--- console/scripts/codegen.sh
+++ console/scripts/codegen.sh
@@ -11,7 +11,7 @@ WELL_KNOWN_CRDS=(
   [Filesystem]='filesystems.scale.spectrum.ibm.com'
   [LocalDisk]='localdisks.scale.spectrum.ibm.com'
   [FusionAccess]='fusionaccesses.fusion.storage.openshift.io'
-  [FailSystemClaim]='filesystemclaims.fusion.storage.openshift.io'
+  [FileSystemClaim]='filesystemclaims.fusion.storage.openshift.io'
   [LocalVolumeDiscoveryResult]='localvolumediscoveryresults.fusion.storage.openshift.io'
 )
 
